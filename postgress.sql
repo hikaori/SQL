aabea41dcdb90b16f4418bb8ff552410b492f6f2
kaori <kaori55194@gmail.com>
2024-07-23 18:47:02 -0700

    add,update,select (#2)

--- postgress.sql
+++ postgress.sql
@@ -11,3 +11,43 @@ band_id INT NOT NULL REFERENCES bands(id) ON DELETE CASCADE
 
 ALTER TABLE album 
 ADD release_year INT;
+
+-- branch2 start.
+
+INSERT INTO bands (name)
+VALUES ('Iron Maiden'), ('Deuce'),('Avenged Sevenfold'),('Ankor');
+
+SELECT * FROM bands;
+
+SELECT * FROM bands LIMIT 2;
+
+SELECT id AS "ID", name AS "Band Name" FROM bands; 
+
+SELECT * FROM bands ORDER BY name DESC;
+
+INSERT INTO album (name, release_year, band_id)
+VALUES ('The number of the Beasts', 1985, 1),
+		('Power Slave', 1984, 1),
+		('Nightmare', 2018, 2),
+		('Nightmare', 2010, 3),
+		('Test Album', NULL , 3);
+SELECT * FROM album;
+
+SELECT * FROM album;
+SELECT DISTINCT name FROM album;
+
+UPDATE album 
+SET release_year = 1982
+WHERE id = 1;
+
+SELECT * FROM album
+WHERE name LIKE '%er%';
+
+SELECT * FROM album
+WHERE name LIKE '%er%' OR band_id = 2 ;
+
+SELECT * FROM album
+WHERE release_year = 1984 AND band_id = 1;
+
+SELECT * FROM album
+WHERE release_year IS NULL;
